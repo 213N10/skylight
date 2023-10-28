6d0f1db50bdac3cbea4d1cc3ccb57e3261f82e6f
it1shka <2tishbel@gmail.com>
2023-10-28 17:40:03 +0200

    Created styles for message form

--- frontend/src/ChatView/index.tsx
+++ frontend/src/ChatView/index.tsx
@@ -1,12 +1,23 @@
 import { useRecoilValue } from 'recoil'
 import { locationState } from './state'
+import * as Styled from './styles'
 import EmptyMock from './EmptyMock'
 
 const ChatView = () => {
-  const location = useRecoilValue(locationState)
+  const location = useRecoilValue(locationState)!
 
   return location ? (
-    <h1>This is a chat view</h1>
+    <Styled.ChatOuterContainer>
+      <Styled.Chat>
+
+      </Styled.Chat>
+      <Styled.ChatForm>
+        <input required
+          placeholder='Write your message here...'
+        />
+        <button type='submit'>Send</button>
+      </Styled.ChatForm>
+    </Styled.ChatOuterContainer>
   ) : <EmptyMock />
 }
 
--- frontend/src/ChatView/styles.ts
+++ frontend/src/ChatView/styles.ts
@@ -14,3 +14,40 @@ export const MockContainer = styled.div`
   line-height: 1.2;
   text-align: center;
 `
+
+export const ChatOuterContainer = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+`
+
+export const Chat = styled.div`
+  flex: 1 1 0;
+  display: flex;
+  flex-direction: column;
+`
+
+export const ChatForm = styled.form`
+  display: flex;
+  font-size: 1.2em;
+  box-shadow: #63636333 0px 2px 8px 0px;
+  border: 1px solid #ccc;
+  & > input {
+    flex: 1;
+    font-size: inherit;
+    border: none;
+    padding: 0.5em 1em;
+    outline: none;
+  }
+  & > button {
+    font-size: inherit;
+    padding: 0 0.5em;
+    color: white;
+    border: none;
+    background-color: #9d9d9d;
+    transition: 0.2s background-color ease-in-out;
+    &:hover {
+      background-color: #c7215b;
+    }
+  }
+`
